0c05a7a13e3d5f654454b15b4aaa9e2879d04c3a
volantrat <volatnrat@gmail.com>
2017-11-29 17:54:46 +0900

    fancy front
    이제 인덱스가 예쁘다.

---
+++ SteganoWEB/static/js/gss.js
@@ -0,0 +1,9 @@
+$('#btn_hide_msg').click(function(){
+    $('#extract_mode_view').hide();
+   $('#hide_mode_view').fadeIn();
+});
+
+$('#btn_extract_msg').click(function(){
+    $('#hide_mode_view').hide();
+   $('#extract_mode_view').fadeIn();
+});
