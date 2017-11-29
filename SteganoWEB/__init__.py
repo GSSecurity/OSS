886b48f75c296225ba1ed5e9e513bfa95af388e7
volantrat <volatnrat@gmail.com>
2017-11-29 15:25:04 +0900

    generate index view
    인덱스 페이지 추가함

---
+++ SteganoWEB/__init__.py
@@ -0,0 +1,2 @@
+import steganography
+import SteganoWEB
--- SteganoWEB/steganography/views.py
+++ SteganoWEB/steganography/views.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+
+def index(request):
+    context = {}
+    template = loader.get_template('../templates/index.html')
+
+    return HttpResponse(template.render(context, request))
