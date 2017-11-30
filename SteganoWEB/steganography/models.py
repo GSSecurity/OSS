cf4011325dc0f980ca0e5dc55c108d966adb34e3
34t3rnull <34t3rnull@naver.com>
2017-11-30 10:14:48 +0900

    Make Model : Save_Info

--- SteganoWEB/steganography/models.py
+++ SteganoWEB/steganography/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Save_Info(models.Model):
+	User_IP = models.CharField(max_length=20)
+	User_Access_Key = models.CharField(max_length=64)
+	User_Agent = models.TextField()
+	Access_Time = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.User_IP
