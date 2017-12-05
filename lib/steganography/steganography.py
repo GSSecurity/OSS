73a3d916fba1d4a9c8fabda51d5646ed40230e71
pwnWiz <pwnwiz@gmail.com>
2017-12-05 23:00:22 +0900

    help 추가 수정

--- lib/steganography/steganography.py
+++ lib/steganography/steganography.py
@@ -238,10 +238,10 @@ def print_help_text():
     print("ERROR: not SteganographyApp command")
     print("--------------------------------")
     print("# encode example: hide text to image")
-    print("SteganographyApp -e /tmp/image/input.jpg /tmp/image/output.jpg 'The quick brown fox jumps over the lazy dog.'")
+    print("SteganographyApp -e /tmp/image/input.jpg /tmp/image/output.jpg 'The quick brown fox jumps over the lazy dog.' 'key")
     print("")
     print("# decode example: read secret text from image")
-    print("SteganographyApp -d /tmp/image/output.jpg")
+    print("SteganographyApp -d /tmp/image/output.jpg 'key:if not match:no result'")
     print("")
 
 if __name__ == "__main__":
